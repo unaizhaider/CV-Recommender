f7dd9ab06625aa9aa0e45a7551f98c6e954f79c2
mtk12 <mtahakhan93@gmail.com>
2019-12-01 12:21:10 +0500

    Login and Signup updated

--- tfidf.py
+++ tfidf.py
@@ -186,8 +186,8 @@ def register():
 @app.route('/login2', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def log():
-    users = db.Job_Seeker
-    user2 = db.Job_Provider 
+    users = Job_Seeker
+    user2 = Job_Provider 
     req=request.get_json(force=True)
     email = req['username']
     password = req['password']
