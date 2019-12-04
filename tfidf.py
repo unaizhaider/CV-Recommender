3ba48ba89cc146e49e987c6d979084ad552e277a
mtk12 <mtahakhan93@gmail.com>
2019-12-04 12:13:05 +0500

    Login and Signup updated

--- tfidf.py
+++ tfidf.py
@@ -182,7 +182,7 @@ def recommend():
     recommended = tfidf(jd,emp_no)
     print(jd)
     print(emp_no)
-    return dumps(recommended)
+    return jsonify(recommended)
 
 @app.route('/register', methods=["POST"])
 @cross_origin(supports_credentials=True)
